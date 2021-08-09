e63360a4f6c139a9af2cdfdff784062f9c983db5
RizqiSyahrendra <rizqialaudinsyahrendra@gmail.com>
2021-08-09 17:09:21 +0700

    remove friend, friend request endpoint

--- controller/friendController.js
+++ controller/friendController.js
@@ -25,8 +25,43 @@ export const addFriend = asyncHandler(async(req, res) => {
         friend_id
     ]);
 
-    return res.json({
+    return res.status(201).json({
         success: true, 
         message: 'add friend success'
+    });
+});
+
+export const removeFriend = asyncHandler(async(req, res) => {
+    let user_id = req.body.user_id;
+    let friend_id = req.body.friend_id;
+
+    await db.query("delete from friends where (user_id = ? and friend_id = ?) or (friend_id = ? and user_id = ?)", [
+        user_id,
+        friend_id,
+        friend_id,
+        user_id
+    ]);
+
+    return res.json({
+        success: true, 
+        message: 'remove friend success'
+    });
+});
+
+export const getFriendRequest = asyncHandler(async(req, res) => {
+    let user_id = req.body.user_id;
+    const friendReq = await db.query(`
+        select *
+        from friends
+        where friend_id = ?
+        and status = 0
+    `, [
+        user_id
+    ]);
+
+    return res.json({
+        success: true, 
+        message: 'get friend request success',
+        data: friendReq
     });
 });
--- routes/friendRoutes.js
+++ routes/friendRoutes.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import {jwtTokenMiddleware} from '../middleware/authMiddleware.js';
 import { 
-    addFriend
+    addFriend, removeFriend,
+    getFriendRequest
 } from '../controller/friendController.js';
 
 const router = express.Router();
 
 router.post('/add', [jwtTokenMiddleware], addFriend);   
+router.delete('/remove', [jwtTokenMiddleware], removeFriend);   
+router.get('/request', [jwtTokenMiddleware], getFriendRequest);   
 
 export default router;
